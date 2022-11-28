be81de8755feff5a55805757cafaf1bce342a4c7
Aaron Garay <aaronroman93@hotmail.com>
2022-11-28 12:18:11 -0300

    Inicio proyecto

---
+++ src/test/java/com/crisalis/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crisalis.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
